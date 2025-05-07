f0719d1695cd80c869f20ef59f99efcef9ad59c2
Shapsoff <84245843+Shapsoff@users.noreply.github.com>
2025-05-07 15:16:42 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import GamePhase from './GamePhase';
 import VotingPhase from './VotingPhase'; // Assurez-vous d'importer le composant VotingPhase
 import Result from './Result';
 
-console.log('Server URL:', process.env.REACT_APP_SERVER_URL);
+console.log('Server URL test 2:', process.env.REACT_APP_SERVER_URL);
 const socket = io(`${process.env.REACT_APP_SERVER_URL}`, {
   transports: ['websocket'], // Force l'utilisation de WebSockets
 });
